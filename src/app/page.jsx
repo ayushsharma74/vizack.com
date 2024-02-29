14e2878251c6005134b47f6faf7f4aec1ffd6d72
Ayush Sharma <134384316+ayushsharma74@users.noreply.github.com>
2024-02-29 12:52:37 +0000

    experimental changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,37 @@
+// "use client"
+// import { Anton } from 'next/font/google';
 
 
-export default function Home() {
+// const anton = Anton({ subsets: ['latin'],weight: "400" });
+
+
+// export default function Home() {
+//   return (
+//     <>
+//       <div className="h-screen w-full flex-col flex justify-center items-center" >
+//         <div className="h-[9.375rem] w-[36.625rem] bg-transparent mr-[21.5rem]">
+//           <main className='bg-green-500'>
+//             <h1 className='text-6xl mb-2'>Hi,we're ViZack Enterprises</h1>
+//           </main>
+//            <h1 className='text-4xl'>We're The Experts Of</h1>
+//          </div>
+//          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-[49.5rem]">
+//           Get In Touch
+//        </button>
+//        </div >
+      
+//        <div className='h-screen w-full bg-red-300 p-10'>
+//          <main className={anton.className}>
+//          <h1 className='text-5'>Our Services.</h1>
+//          </main>
+//        </div>
+//      </>
+//    );
+//  }
+export default function home(){
   return (
     <>
+    
     </>
-  );
+  )
 }
