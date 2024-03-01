77e2e2c77d9d1478b0258990be17c5668c6162af
Ayush Sharma <134384316+ayushsharma74@users.noreply.github.com>
2024-03-01 15:33:25 +0000

    added typing effect (change later)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { Anton } from 'next/font/google';
 import Card from "@/components/Card"
+import { Typewriter } from "react-simple-typewriter"
 
 
 const anton = Anton({ subsets: ['latin'],weight: "400" });
@@ -39,7 +40,7 @@ export default function home(){
               <h1 className='text-5xl mb-2'>Hi,we're <font><span class name="text-default">V</span>i<span class name="text-default">Z</span>ack Enterprises</font></h1>
             </main>
             <font className={anton.className}>
-             <h1 className='text-4xl mt-3'>We're The <em className='normal font-serif'> Experts </em> of</h1>
+             <h1 className='text-4xl mt-3'>We're The <em className='normal font-serif'> Experts </em> of <Typewriter words={['Web development','Data analytics','Strategic sales','Brand building']} cursor='true' loop='false'/></h1>
              </font>
           </div>
            <button className="bg-transparent hover:bg-[#f2932f] text-[#f2932f] font-semibold hover:text-white py-2 px-5 border border-[#f2932f] hover:border-transparent rounded mr-[49.5rem] absolute bottom-[13.75rem] transition ease-in-out duration-300">
