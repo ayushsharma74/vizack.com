8e0a2d732b64cf2c58e6bdb158581bab2b196800
Ayush Sharma <134384316+ayushsharma74@users.noreply.github.com>
2024-03-09 15:10:53 +0000

    add service section

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,109 +1,50 @@
 "use client"
-import Servicecard from "@/components/Servicecard";
 // import Link from "next/link";
 import Upperbanner from "@/components/Upperbanner";
+import Content from "@/components/Servicecard";
 // import about from "../about/page";
-export default function services(){
-    return(
+export default function services() {
+    return (
         <>
-       
-        <Upperbanner content={'SERVICES'} route={'services'} routeName={'Services'}/>
-        <div className="mb-10">
-            <Servicecard 
-                    service={'Digital Discovery Dive'}
-                    content={`
-                    Embark on a digital voyage of discovery with our 
-expert-guided sessions. Dive deep into the digital landscape, uncovering 
-hidden opportunities and charting a course for your brand's success. 
-Explore new horizons, harnessing the power of data-driven insights and 
-innovative strategies to propel your business to greater heights.`}
-                    imgURL="/assets/images/digital dive.jpeg"
-                />
-        </div>
-        <div className="mb-10">
-            <Servicecard 
-                    service={'Website Design and Development'}
-                    content={`
-                Transform your online presence into a 
-                digital masterpiece with our website design and development services. 
-                From concept to creation, we craft visually stunning, user-friendly websites 
-                that captivate audiences and drive results. Elevate your brand with a 
-                website that not only looks great but also delivers an exceptional user 
-                experience. 
-                    `}
-                    imgURL="/assets/images/web development.jpeg"
-                />
-
-            </div>
-            <div className="mb-10">
-                <Servicecard
-                    service={'Public Relations'}
-                    content={`
-                    Ignite conversations, shape perceptions, and build lasting 
-relationships with our strategic public relations solutions. From media 
-relations to reputation management, we amplify your brand's voice, 
-ensuring it resonates with the right audiences at the right time. Navigate 
-the ever-changing media landscape with confidence, guided by our 
-seasoned PR experts.
-                    `}
-                    imgURL="/assets/images/public relations.jpg"
-                />
-            </div>
-            <div className="mb-10">
-                <Servicecard
-                    service={'Data Analytics Services'}
-                    content={`
-                    Unlock the power of data with our comprehensive 
-analytics services. From data collection to actionable insights, we help you 
-harness the full potential of your data assets. Dive deep into the numbers, 
-uncovering hidden patterns and trends that drive informed decision-making and fuel business growth. Turn data into your competitive 
-advantage with our advanced analytics solutions.
-                    `}
-                    imgURL="/assets/images/data analytics services.png"
-                />
-        </div>
-        <div className="mb-10">
-            <Servicecard 
-                    service={'Strategic Sales Solutions'}
-                    content={`
-                    Revolutionize your sales approach with our 
-strategic sales solutions. From lead generation to conversion optimization, 
-we craft tailored strategies that drive results. Break through barriers, unlock 
-new markets, and accelerate your sales pipeline with our innovative 
-approaches and proven methodologies. Elevate your sales game and 
-achieve sustainable growth with our strategic guidance.
-                    `}
-                    imgURL="/assets/images/strategic solution.png"
-                />
-        </div>
-        <div className="mb-10">
-                <Servicecard
-                    service={'Innovative Brand Building'}
-                    content={`
-                    Build a brand that stands out in a crowded 
-marketplace with our innovative brand building services. From brand 
-strategy to creative execution, we craft compelling narratives and 
-memorable experiences that resonate with your target audience. Transform 
-your brand into a beacon of inspiration, driving loyalty, engagement, and 
-long-term success. Unleash the full potential of your brand with our 
-innovative approach to branding.
-                    `}
-                    imgURL="/assets/images/brand building.webp"
-                />
-        </div>
-        <div className="mb-10">
-                <Servicecard
-                    service={'Strategic Politics Redefined'}
-                    content={`
-                    Empowering political leaders, candidates, 
-and organizations with strategic guidance and innovative solutions is our 
-mission. Our experienced team crafts compelling narratives and winning 
-campaign strategies tailored to your specific goals. With a focus on 
-integrity and transparency, we partner with those committed to positive 
-change, shaping the future of politics together.
-                    `}
-                    imgURL="/assets/images/politics redefined.png"
-                />
+            <Upperbanner content={'SERVICES'} route={'services'} routeName={'Services'} />
+            <div>
+                <Content image_src={"/assets/testing/5484597.jpg"} service={"Website Design & Development"} content={`Transform your online presence into a
+                    digital masterpiece with our website design and development services.
+                    From concept to creation, we craft visually stunning, user-friendly websites
+                    that captivate audiences and drive results. Elevate your brand with a
+                    website that not only looks great but also delivers an exceptional user
+                    experience.`} />
+                <Content image_src={"/assets/testing/7038058.jpg"} service={"Public Relations"} content={`Ignite conversations, shape perceptions, and build lasting
+                    relationships with our strategic public relations solutions. From media
+                    relations to reputation management, we amplify your brand's voice,
+                    ensuring it resonates with the right audiences at the right time. Navigate
+                    the ever-changing media landscape with confidence, guided by our
+                    seasoned PR experts.`} />
+                <Content image_src={"/assets/testing/2598560c.jpg"} service={"Data Analytics"} content={` Unlock the power of data with our comprehensive
+                    analytics services. From data collection to actionable insights, we help you
+                    harness the full potential of your data assets. Dive deep into the numbers,
+                    uncovering hidden patterns and trends that drive informed decision-
+                    making and fuel business growth. Turn data into your competitive
+                    advantage with our advanced analytics solutions.`} />
+                <Content image_src={"/assets/testing/7039196.jpg"} service={"Strategic Sales"} content={`Revolutionize your sales approach with our
+                    strategic sales solutions. From lead generation to conversion optimization,
+                    we craft tailored strategies that drive results. Break through barriers, unlock
+                    new markets, and accelerate your sales pipeline with our innovative
+                    approaches and proven methodologies. Elevate your sales game and
+                    achieve sustainable growth with our strategic guidance.`} />
+                <Content image_src={"/assets/testing/7634517.jpg"} service={"Innovative Brand Building"} content={`Build a brand that stands out in a crowded
+                    marketplace with our innovative brand building services. From brand
+                    strategy to creative execution, we craft compelling narratives and
+                    memorable experiences that resonate with your target audience. Transform
+                    your brand into a beacon of inspiration, driving loyalty, engagement, and
+                    long-term success. Unleash the full potential of your brand with our
+                    innovative approach to branding.`} />
+                <Content image_src={"/assets/testing/5458281.jpg"} service={"Strategic Politics"} content={`Empowering political leaders, candidates,
+                    and organizations with strategic guidance and innovative solutions is our
+                    mission. Our experienced team crafts compelling narratives and winning
+                    campaign strategies tailored to your specific goals. With a focus on
+                    integrity and transparency, we partner with those committed to positive
+                    change, shaping the future of politics togethe`} />
             </div>
         </>
     );
--- src/components/Servicecard.jsx
+++ src/components/Servicecard.jsx
@@ -1,43 +1,40 @@
-// 
 
-const image = "/assets/testing/5484597.jpg"
 
-export default function Servicecard({ service, content, imgURL, css }) {
+export default function Servicecard({image_src,service,content}) {
   return (
-    <section>
-      <div className="px-2 lg:flex lg:flex-row lg:items-center">
-        <div className="w-full lg:w-1/2">
-          <div className="my-10 lg:my-0 lg:px-10">
-            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
-              {service}
-            </h2>
-            <p className="mt-4 max-w-xl text-base leading-relaxed text-gray-600">
-              {content}
-            </p>
-            <form action="#" method="POST" className="mt-8 max-w-xl">
-              <div className="flex flex-col sm:flex-row sm:items-center">
-                <div className="flex w-full max-w-sm items-center space-x-2">
-                  <button
-                    type="button"
-                    className="rounded-md w-[8.5rem] h-[2.5rem] bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                  >
-                    Learn more
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div className="p-8 rounded shadow-xl sm:p-16">
+      <div className="flex flex-col lg:flex-row">
+        <div className="mb-6 lg:mb-0 lg:w-1/4 lg:pr-5">
+          {/* <h2 className="font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+            The quick, brown fox
+            <br className="hidden md:block" />
+            jumps over{' '}
+            <span className="inline-block text-deep-purple-accent-400">
+              a lazy dog
+            </span>
+          </h2> */}
+          <img src={image_src} alt="" />
         </div>
-        <div className="">
-          <img
-            // src="https://images.unsplash.com/photo-1603575448878-868a20723f5d?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDN8fGRldmVsb3BlcnxlbnwwfHwwfHw%3D&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-            src={imgURL}
-            alt={service}
-            className={css}
-          />
+        <div className="lg:w-3/4 flex flex-col justify-center">
+            <h1 className="text-4xl mb-3">{service}</h1>
+          <p className="mb-4 text-base text-gray-700">
+            {content}
+          </p>
+          <a
+            href="/"
+            aria-label=""
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          >
+            <div class="relative inline-block overflow-hidden bg-hsl-222-100-95 text-hsl-243-80-62">
+            <button type="submit" class="inline-flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded text-white bg-default hover:bg-orange-500 transition-colors">Learn more</button>
+            </div>
+
+          </a>
         </div>
       </div>
-    </section>
+    </div>
+  </div>
 
   );
 }
