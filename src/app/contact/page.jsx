b55fe132f4342691cf2e2a70e40e12c9c87486d9
Ayush Sharma <134384316+ayushsharma74@users.noreply.github.com>
2024-03-10 11:57:47 +0000

    Added API For contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '@/styles/globals.css'
+import ContactPage from '@/components/ContactPage'
 
 const contact = () => {
   return (
     <>
-      <section className="bg-gray-900 body-font relative">
+      {/* <section className="bg-gray-900 body-font relative">
         <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
           <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
             <iframe
@@ -45,7 +46,8 @@ const contact = () => {
             <p className="text-xs text-white mt-3">Thank you for reaching out! we will soon get back to you.</p>
           </div>
         </div>
-      </section>
+      </section> */}
+      <ContactPage />
     </>
   )
 }
