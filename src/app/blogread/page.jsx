b5394f23e7e45600e272e22a2ceb336e9e209e65
Gajender <Gajendrasuman868@gmail.com>
2024-03-11 12:18:29 +0000

    sfdfd

--- src/app/blogread/page.jsx
+++ src/app/blogread/page.jsx
@@ -130,16 +130,16 @@ export default function Breader() {
                                             <div className="tags flex gap-2 items-center">
                                                 <h1 className="text-dark text-xl font-semibold">Share: </h1>
                                                 <div className="flex flex-wrap gap-2 px-4 flex-auto py-6">
-                                                    {/* <Link rel="noopener noreferrer" href={"https://www.facebook.com/sharer/sharer.php?u=https://vizackcom.vercel.app/blogread"} target="_blank" className="hover:text-[#ff6900] transition-colors duration-300" >Facebook</Link>
+                                                    <Link rel="noopener noreferrer" href={"https://www.facebook.com/sharer/sharer.php?u=https://vizackcom.vercel.app/blogread"} target="_blank" className="hover:text-[#ff6900] transition-colors duration-300" >Facebook</Link>
                                                     <Link rel="noopener noreferrer" href={"https://twitter.com/intent/tweet?url=https://vizackcom.vercel.app/blogread&text=Check%20out%20my%20blog%20post"} target="_blank" className="hover:text-[#ff6900] transition-colors duration-300" >X</Link>
                                                     <Link rel="noopener noreferrer" href={"https://www.linkedin.com/shareArticle?url=https://vizackcom.vercel.app/blogread&title="} target="_blank" className="hover:text-[#ff6900] transition-colors duration-300" >Linkedin</Link>
                                                     <Link rel="noopener noreferrer" href={"whatsapp://send?text=https://vizackcom.vercel.app/blogread"} data-action="share/whatsapp/share" target="_blank" className="hover:text-[#ff6900] transition-colors duration-300" >Whatsapp</Link>
-                                                 */}
-                                                 <Link rel="noopener noreferrer" href={"/"} target="_blank" className="hover:text-[#ff6900] transition-colors duration-300" >Facebook</Link>
+                                                
+                                                 {/* <Link rel="noopener noreferrer" href={"/"} target="_blank" className="hover:text-[#ff6900] transition-colors duration-300" >Facebook</Link>
                                                     <Link rel="noopener noreferrer" href={"/"} target="_blank" className="hover:text-[#ff6900] transition-colors duration-300" >X</Link>
                                                     <Link rel="noopener noreferrer" href={"/"} target="_blank" className="hover:text-[#ff6900] transition-colors duration-300" >Linkedin</Link>
                                                     <Link rel="noopener noreferrer" href={"/"} data-action="share/whatsapp/share" target="_blank" className="hover:text-[#ff6900] transition-colors duration-300" >Whatsapp</Link>
-                                                
+                                                 */}
                                                 </div>
 
                                             </div>
@@ -162,10 +162,10 @@ export default function Breader() {
                                                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam no eirmod tempor invidunt ut labore et magna aliquyam erat.
                                             </p>
                                             <div className="social flex gap-4">
-                                                <Link href={"https://"}><i className="text-[#005EF9] hover:text-default text-xl transition-colors duration-300 icofont-facebook"></i></Link>
-                                                <Link href={"https://"}><i className="text-[#005EF9] hover:text-default text-xl transition-colors duration-300 icofont-twitter"></i></Link>
-                                                <Link href={"https://"}><i className="text-[#005EF9] hover:text-default text-xl transition-colors duration-300 icofont-linkedin"></i></Link>
-                                                <Link href={"https://"}><i className="text-[#005EF9] hover:text-default text-xl transition-colors duration-300 icofont-instagram"></i></Link>
+                                                <Link href={"https://facebook.com"}><i className="text-[#005EF9] hover:text-default text-xl transition-colors duration-300 icofont-facebook"></i></Link>
+                                                <Link href={"https://twitter.com"}><i className="text-[#005EF9] hover:text-default text-xl transition-colors duration-300 icofont-twitter"></i></Link>
+                                                <Link href={"https://linkedin.com"}><i className="text-[#005EF9] hover:text-default text-xl transition-colors duration-300 icofont-linkedin"></i></Link>
+                                                <Link href={"https://instagram.com"}><i className="text-[#005EF9] hover:text-default text-xl transition-colors duration-300 icofont-instagram"></i></Link>
                                             </div>
                                         </div>
                                     </div>
