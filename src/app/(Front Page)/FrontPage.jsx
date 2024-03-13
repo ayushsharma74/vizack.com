e2e6febf7fe6db748178e4585ee0f73707b8477d
farhankhan197 <146377678+farhankhan197@users.noreply.github.com>
2024-03-13 05:23:14 +0000

    frontpage responsiveness and normal alignment fixes

--- src/app/(Front Page)/FrontPage.jsx
+++ src/app/(Front Page)/FrontPage.jsx
@@ -1,23 +1,20 @@
-'use client'
 import { Anton } from 'next/font/google';
 import { Typewriter } from "react-simple-typewriter";
 import Image from 'next/image';
-import Link from 'next/link';
 const anton = Anton({ subsets: ['latin'], weight: "400" });
-
 
 export default function Mainpage() {
   return (
     <>
-      <div className="bg-white min-h-screen font-sans flex flex-col items-center justify-center">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="lg:grid lg:grid-cols-12 lg:gap-8 pt-12 lg:pt-0">
+      <div className="bg-white h-60 mt-44 mb-44 font-sans flex flex-col items-center justify-center py-8">
+        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-12 lg:gap-8 pt-6 lg:pt-0">
             <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left lg:flex lg:items-center">
               <div className="flex flex-col items-center justify-center">
                 <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-gray-900 sm:mt-5 sm:leading-none lg:mt-6 lg:text-5xl xl:text-6xl">
-                  <p className="sm:block mb-4">Welcome to</p>
+                  <p className="sm:block ">Welcome to</p>
                   <span className="text-orange-600 mb-8 md:block">ViZack Enterprises</span>
-                  <p className="text-zinc-500 md:block">Masters of {''}</p>
+                  <p className="text-zinc-500 md:block mt-4">Masters of {''}</p>
                   <span>
                     <Typewriter
                       words={['Data Analytics', 'Strategic Sales', 'Public Relations', 'Web Design', 'Web Development', 'Digital Marketing']}
@@ -30,29 +27,26 @@ export default function Mainpage() {
                   </span>
                 </h1>
                 <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
-                  <Link href={"/services"}><button className="inline-flex items-center text-white bg-orange-500 justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full sm:w-auto">
+                  <button className="inline-flex items-center text-white bg-orange-500 justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full sm:w-auto mb-3 sm:mb-0 sm:mr-3">
                     Our Services
                   </button>
-                  </Link>
-                  <Link href={"/contact"}>
-                  <button className="inline-flex hover:ring-gray-400 hover:bg-gray-100 items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 mt-3 w-full sm:mt-0 sm:ml-3 sm:w-auto">
+                  <button className="inline-flex hover:ring-gray-400 hover:bg-gray-100 items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 w-full sm:w-auto">
                     Contact Us
                   </button>
-                  </Link>
                 </div>
               </div>
             </div>
-            <div className="mt-16 ml-6 sm:mt-24 lg:mt-0 lg:col-span-5 flex items-center">
+            <div className="mt-8 sm:mt-0 lg:mt-0 lg:col-span-5 flex items-center justify-center">
               <div className="mr-6">
                 <Image src="/assets/images/asset6.jpg" alt="Image" width={1000} height={1000} />
               </div>
               <p className="text-base text-gray-600 sm:text-xl lg:text-lg xl:text-xl">
-
+                {/* Placeholder text */}
               </p>
             </div>
           </div>
         </div>
-      </div >
+      </div>
     </>
   );
 }
