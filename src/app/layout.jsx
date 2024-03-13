99df2743e0015d79dd686d986327731c72529507
Gajender <Gajendrasuman868@gmail.com>
2024-03-13 18:05:14 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,10 +6,25 @@ import NextTopLoader from 'nextjs-toploader';
 import Footer from "@/components/Footer";
 import ToastProvider from "@/components/ToastProvider";
 
-// export const metadata = {
-//   title: "Vizack Enterprises",
-//   description: "Welcome to ViZack Enterprises Pvt Ltd - where your success is our passion, where commitment meets excellence. At ViZack Enterprises, we pride ourselves on being more than just a startup organization - we're a dedicated team of professionals driven by a singular mission: to provide unparalleled customer service and empower small businesses to achieve remarkable growth.",
-// };
+export const metadata = {
+  title: "Vizack Enterprises",
+  description: "Welcome to ViZack Enterprises Pvt Ltd - where your success is our passion, where commitment meets excellence. At ViZack Enterprises, we pride ourselves on being more than just a startup organization - we're a dedicated team of professionals driven by a singular mission: to provide unparalleled customer service and empower small businesses to achieve remarkable growth.",
+  ogTitle: "ViZack Enterprises Pvt Ltd",
+  ogDescription: "Welcome to ViZack Enterprises Pvt Ltd - where your success is our passion, where commitment meets excellence. At ViZack Enterprises, we pride ourselves on being more than just a startup organization - we're a dedicated team of professionals driven by a singular mission: to provide unparalleled customer service and empower small businesses to achieve remarkable growth.",
+  ogImage: "/assets/logo/logo.png",
+  ogUrl: "https://www.vizackenterprises.com",
+  ogType: "website",
+  ogLocale: "en_US",
+  author: "ViZack Enterprises Pvt Ltd",
+  twitterCard: "summary_large_image",
+  twitterTitle: "ViZack Enterprises Pvt Ltd",
+  twitterDescription: "Welcome to ViZack Enterprises Pvt Ltd - where your success is our passion, where commitment meets excellence. At ViZack Enterprises, we pride ourselves on being more than just a startup organization - we're a dedicated team of professionals driven by a singular mission: to provide unparalleled customer service and empower small businesses to achieve remarkable growth.",
+  twitterImage: "/assets/logo/logo.png",
+  twitterUrl: "https://twitter.com/ViZackEnterpise",
+  robots: "index, imageindex, follow, max-snippet:50, max-video-preview:10, max-image-preview:large, nocache",
+  robotsUrl: "/robots.txt",
+  keywords: "ViZack, vizack, enterprises, vizack enterprises, pvt, Pvt, ltd, Ltd, vizack enterprises pvt,vizack enterprises pvt ltd, the ViZack, Digital, marketing, digital marketing, website, web, development, web development, website development, hosting, create website, make website, want digital, want digital marketing"
+};
 
 export default function RootLayout({ children }) {
   
@@ -17,26 +32,10 @@ export default function RootLayout({ children }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta name="author" content="ViZack Enterprises Pvt Ltd" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="ViZack Enterprises Pvt Ltd" />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content="/assets/logo/logo.png" />
-        <meta name="twitter:url" content="https://twitter.com/ViZackEnterpise" />
-        <meta property="og:title" content="ViZack Enterprises Pvt Ltd" />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="/assets/logo/logo.png" />
-        <meta property="og:url" content="https://www.vizackenterprises.com" />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="en_US" />
-        <meta name="descrption" content="Welcome to ViZack Enterprises Pvt Ltd - where your success is our passion, where commitment meets excellence. At ViZack Enterprises, we pride ourselves on being more than just a startup organization - we're a dedicated team of professionals driven by a singular mission: to provide unparalleled customer service and empower small businesses to achieve remarkable growth."/>
-        <title>Vizack Enterprises Pvt Ltd</title>
-        <meta name="robots" content="index, imageindex, follow, max-snippet:50, max-video-preview:10, max-image-preview:large, nocache" />
-        <meta name="keywords" content="ViZack, vizack, enterprises, vizack enterprises, pvt, Pvt, ltd, Ltd, vizack enterprises pvt,vizack enterprises pvt ltd, the ViZack, Digital, marketing, digital marketing, website, web, development, web development, website development, hosting, create website, make website, want digital, want digital marketing" />
         <link rel="canonical" href="https://www.vizackenterprises.com" />
         <link rel="alternate" href="https://www.vizackenterprises.com" hreflang="x-default" />
         <link rel="alternate" href="https://www.vizackenterprises.com" hreflang="en-us" />
-        <link rel="shortcut icon" href="/assets/logo/logo.png" type="image/x-icon" />
+        <link rel="icon" href="/assets/logo/logo.png" />
       </Head>
       <body className="max-w-full overflow-x-hidden">
         <ToastProvider>
