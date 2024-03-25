21c5dde9010074be2fcbf906809a7b11a45645bc
Gajender <Gajendrasuman868@gmail.com>
2024-03-25 16:24:45 +0000

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-module.exports = {
+const exports = {
     api: {
         bodyParser: {
             sizeLimit: '10mb', // Adjust size limit as needed
         },
     }
 };
-   
+export default exports;
