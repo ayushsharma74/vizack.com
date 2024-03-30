ec56463cbc250c95aa85a7925336363713ac1b57
Gajender <Gajendrasuman868@gmail.com>
2024-03-30 09:51:54 +0000

    service border

--- src/components/Services2.jsx
+++ src/components/Services2.jsx
@@ -22,8 +22,7 @@ const Services2 = () => {
           <div className="ml-0 md:ml-12 lg:w-2/3 sticky">
             <div className="container mx-auto w-full h-full">
               <div className="relative wrap overflow-hidden p-10 pb-0 mb-10 h-fit">
-                <div className="border-2-2 border-yellow-555 absolute h-full border"
-                  style={{ right: '50%', border: '4px solid #FFC100', borderSpacing: "2px", borderRadius: '1%' }}></div>
+                <div className="border-r-8 right-1/2 border-yellow-555 absolute h-full border"></div>
                 <div className="mb-1 flex justify-between flex-row-reverse items-center w-full left-timeline">
                   <div className="order-1 w-5/12"></div>
                   <div className="order-1 w-5/12 px-1 py-4 text-right">
@@ -49,8 +48,7 @@ const Services2 = () => {
               </div>
 
               <div className="relative wrap overflow-hidden p-10 pb-0 mb-10 h-full">
-                <div className="border-2-2 border-yellow-555 absolute h-full border"
-                  style={{ right: '50%', border: '4px solid #FFC100', borderSpacing: "2px", borderRadius: '1%' }}></div>
+                <div className="border-r-8 right-1/2 border-yellow-555 absolute h-full border"></div>
                 <div className="mb-1 flex justify-between flex-row-reverse items-center w-full left-timeline">
                   <div className="order-1 w-5/12"></div>
                   <div className="order-1 w-5/12 px-1 py-4 text-right">
@@ -77,8 +75,7 @@ const Services2 = () => {
               </div>
             </div>
             <div className="relative wrap overflow-hidden p-10 pb-0 mb-10 h-fit">
-              <div className="border-2-2 border-yellow-555 absolute h-full border"
-                style={{ right: '50%', border: '4px solid #FFC100', borderSpacing: "2px", borderRadius: '1%' }}></div>
+              <div className="border-r-8 right-1/2 border-yellow-555 absolute h-full border"></div>
               <div className="mb-1 flex justify-between flex-row-reverse items-center w-full left-timeline">
                 <div className="order-1 w-5/12"></div>
                 <div className="order-1 w-5/12 px-1 py-4 text-right">
@@ -104,8 +101,7 @@ const Services2 = () => {
             </div>
             <div className="container mx-auto w-full h-full">
               <div className="relative wrap overflow-hidden p-10 pb-0 mb-10 h-fit">
-                <div className="border-2-2 border-yellow-555 absolute h-full border"
-                  style={{ right: '50%', border: '4px solid #FFC100', borderSpacing: "2px", borderRadius: '1%' }}></div>
+                <div className="border-r-8 right-1/2 border-yellow-555 absolute h-full border"></div>
                 <div className="mb-1 flex justify-between flex-row-reverse items-center w-full left-timeline">
                   <div className="order-1 w-5/12"></div>
                   <div className="order-1 w-5/12 px-1 py-4 text-right">
