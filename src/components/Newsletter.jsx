d64ba28872991263edc40c62bfcaa3da98aab311
Gajender <Gajendrasuman868@gmail.com>
2024-03-31 13:58:11 +0530

    Update Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -9,7 +9,9 @@ const Newsletter = () => {
     })
   }
   return (
+    <>
     <div className="visme_d" data-title="ViZack Subscription form" data-url="8r6dmqvq-vizack-subscription-form" data-domain="forms" data-full-page="false" data-min-height="500px" data-form-id="52195"></div><script src="https://static-bundles.visme.co/forms/vismeforms-embed.js"></script>  
+    </>
 
 {/*       <div className="slide-container">
         <Slide>
