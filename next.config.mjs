0ebaa412670f90488f48b4a52dfe6af7b6af8138
Gajender suman <Gajendrasuman868@gmail.com>
2024-04-01 06:53:40 +0000

    deleted test routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-const exports = {
-    api: {
-        bodyParser: {
-            sizeLimit: '10mb', // Adjust size limit as needed
-        },
-    }
-};
-export default exports;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
