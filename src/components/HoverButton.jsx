d6947dd8f5b3ccd75b7eb25badd151cccc47cf15
farhankhan197 <146377678+farhankhan197@users.noreply.github.com>
2024-04-06 03:18:21 +0000

    service page Hover button fix

--- src/components/HoverButton.jsx
+++ src/components/HoverButton.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 const HoverButton = () => {
     return (
-      <Link href= 'https://forms.gle/yLADgf8vtnQEx4Ac8'>  
-      <button className=" group relative h-12 w-48 overflow-hidden rounded bg-white text-lg shadow-xl">
+      <Link href= 'https://forms.gle/yLADgf8vtnQEx4Ac8' target="_blank">  
+      <button className=" group relative h-12 w-48 overflow-hidden rounded bg-white text-lg shadow-xl" >
         <div className="absolute inset-0 w-3 bg-default transition-all duration-[250ms] ease-out group-hover:w-full"></div>
         <span className="font-bold  text-sm relative text-black group-hover:text-white">Get Started</span>
       </button>
