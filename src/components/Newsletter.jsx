39780378b5c29818f8805524c82b1b966b767c8f
Gajender <Gajendrasuman868@gmail.com>
2024-04-09 15:48:27 +0530

    Update Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -17,7 +17,7 @@ const Newsletter = () => {
           <form id="sib-form" method="POST" action="https://a9812ab7.sibforms.com/serve/MUIFAP3bgyQYoAO9-Ufhi8eT0h33LxFYJaqxzKjE3BTfDQZ--tPAd1IU2TFVCbcLiv6RJVfLWX9uBsSOfmaLvJ3h8PN81Do5Nq51Ftu2QtuC-JhGpKXXvDWt-rqIY3hLVLNUiQ46TEtOGvA_CSNHeWQnLkWhXUik0QM0_Vr2hnS2iL8LnT_lyVNaEz0niMUAfPq49Ovwx1GaNdKp" className="mx-auto mt-4 flex w-full max-w-md flex-col gap-3 sm:flex-row sm:gap-0">
             <input type="email" id="EMAIL" name="EMAIL" autocomplete="off" data-required="true" required className="grow rounded border-2 mr-2 border-black py-3 px-3 focus:border-emerald-500 focus:outline-none sm:rounded-l-md  " placeholder="Email Address" />
             <button onClick={handleClick} type="submit" form='sib-form' className="rounded bg-orange-600 px-5 py-4 font-bold text-white hover:bg-white hover:text-orange-600  transition-colors ease-in-out delay-75" >Get First Email</button>
-            <input type="hidden" name="email_address_check" value="" class="input--hidden" />
+            <input type="hidden" name="email_address_check" value=""  className="input-hidden" />
             <input type="hidden" name="locale" value="en"/>
             <input type="hidden" name="html_type" value="simple"/>
           </form>
