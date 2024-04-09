e47d3ab78bf5274d3526591b7ee7767f739cce1d
Gajender <Gajendrasuman868@gmail.com>
2024-04-09 15:41:32 +0530

    Update FunFactsSection.jsx

--- src/app/(Front Page)/FunFactsSection.jsx
+++ src/app/(Front Page)/FunFactsSection.jsx
@@ -3,38 +3,38 @@ import CountUp from 'react-countup';
 
 function Funfactssection() {
   return (
-    <div class="bg-white py-6 sm:py-8 lg:py-12">
-        <div class="mx-auto max-w-screen-xl px-4 md:px-8">
-          <div class="mb-8 md:mb-12">
-            <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Our Team by the numbers</h2>
+    <div  className="bg-white py-6 sm:py-8 lg:py-12">
+        <div  className="mx-auto max-w-screen-xl px-4 md:px-8">
+          <div  className="mb-8 md:mb-12">
+            <h2  className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Our Team by the numbers</h2>
 
-            <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg"></p>
+            <p  className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg"></p>
           </div>
 
 
-          <div class="grid grid-cols-2 gap-4 md:grid-cols-4 lg:gap-8">
+          <div  className="grid grid-cols-2 gap-4 md:grid-cols-4 lg:gap-8">
 
-            <div class="flex flex-col items-center justify-center rounded-lg bg-gray-100 p-4 lg:p-8">
-              <div class="text-xl font-bold text-default sm:text-2xl md:text-3xl"><CountUp end={2} enableScrollSpy/>+</div>
-              <div class="text-sm font-semibold sm:text-base">Branches</div>
+            <div  className="flex flex-col items-center justify-center rounded-lg bg-gray-100 p-4 lg:p-8">
+              <div  className="text-xl font-bold text-default sm:text-2xl md:text-3xl"><CountUp end={2} enableScrollSpy/>+</div>
+              <div  className="text-sm font-semibold sm:text-base">Branches</div>
             </div>
 
-            <div class="flex flex-col items-center justify-center rounded-lg bg-gray-100 p-4 md:p-8">
-              <div class="text-xl font-bold text-default sm:text-2xl md:text-3xl"><CountUp end={50} enableScrollSpy/>+</div>
-              <div class="text-sm font-semibold sm:text-base">Employees</div>
-            </div>
-
-
-            <div class="flex flex-col items-center justify-center rounded-lg bg-gray-100 p-4 md:p-8">
-              <div class="text-xl font-bold text-default sm:text-2xl md:text-3xl"><CountUp end={500} enableScrollSpy/>+</div>
-              <div class="text-sm font-semibold sm:text-base">Happy clients</div>
+            <div  className="flex flex-col items-center justify-center rounded-lg bg-gray-100 p-4 md:p-8">
+              <div  className="text-xl font-bold text-default sm:text-2xl md:text-3xl"><CountUp end={50} enableScrollSpy/>+</div>
+              <div  className="text-sm font-semibold sm:text-base">Employees</div>
             </div>
 
 
+            <div  className="flex flex-col items-center justify-center rounded-lg bg-gray-100 p-4 md:p-8">
+              <div  className="text-xl font-bold text-default sm:text-2xl md:text-3xl"><CountUp end={500} enableScrollSpy/>+</div>
+              <div  className="text-sm font-semibold sm:text-base">Happy clients</div>
+            </div>
 
-            <div class="flex flex-col items-center justify-center rounded-lg bg-gray-100 p-4 md:p-8">
-              <div class="text-xl font-bold text-default sm:text-2xl md:text-3xl"><CountUp end={45} enableScrollSpy/>+</div>
-              <div class="text-sm font-semibold sm:text-base">Projects</div>
+
+
+            <div  className="flex flex-col items-center justify-center rounded-lg bg-gray-100 p-4 md:p-8">
+              <div  className="text-xl font-bold text-default sm:text-2xl md:text-3xl"><CountUp end={45} enableScrollSpy/>+</div>
+              <div  className="text-sm font-semibold sm:text-base">Projects</div>
             </div>
 
           </div>
