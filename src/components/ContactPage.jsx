d9f0a5c30c63337a09a6a782887f316964c9d811
Gajender <Gajendrasuman868@gmail.com>
2024-04-09 15:46:06 +0530

    Update ContactPage.jsx

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -32,43 +32,43 @@ export default function ContactPage(){
         }
     }
     return(
-        <div class="mx-auto max-w-7xl px-4">
-        <div class="mx-auto max-w-7xl py-12 md:py-24">
-          <div class="grid items-center justify-items-center gap-x-4 gap-y-10 lg:grid-cols-2">
-            <div class="flex items-center justify-center">
-              <div class="px-2 md:px-12">
-                <p class="text-2xl font-bold text-gray-900 md:text-4xl">
+        <div  className="mx-auto max-w-7xl px-4">
+        <div  className="mx-auto max-w-7xl py-12 md:py-24">
+          <div  className="grid items-center justify-items-center gap-x-4 gap-y-10 lg:grid-cols-2">
+            <div  className="flex items-center justify-center">
+              <div  className="px-2 md:px-12">
+                <p  className="text-2xl font-bold text-gray-900 md:text-4xl">
                   Get in touch
                 </p>
-                <p class="mt-4 text-lg text-gray-600">
+                <p  className="mt-4 text-lg text-gray-600">
                   Our friendly team would love to hear from you.
                 </p>
-                <form action="" class="mt-8 space-y-4" onSubmit={handleSubmit}>
-                  <div class="grid w-full gap-y-4 md:gap-x-4 lg:grid-cols-2">
-                    <div class="grid w-full  items-center gap-1.5">
+                <form action=""  className="mt-8 space-y-4" onSubmit={handleSubmit}>
+                  <div  className="grid w-full gap-y-4 md:gap-x-4 lg:grid-cols-2">
+                    <div  className="grid w-full  items-center gap-1.5">
                       <label
-                        class="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                         className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                         for="first_name"
                       >
                         First Name
                       </label>
                       <input
-                        class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                         className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                         type="text"
                         name="name"
                         id="first_name"
                         placeholder="First Name"
                       />
                     </div>
-                    <div class="grid w-full  items-center gap-1.5">
+                    <div  className="grid w-full  items-center gap-1.5">
                       <label
-                        class="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                         className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                         for="last_name"
                       >
                         Last Name
                       </label>
                       <input
-                        class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                         className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                         type="text"
                         name="name"
                         id="last_name"
@@ -76,45 +76,45 @@ export default function ContactPage(){
                       />
                     </div>
                   </div>
-                  <div class="grid w-full  items-center gap-1.5">
+                  <div  className="grid w-full  items-center gap-1.5">
                     <label
-                      class="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                       className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                       for="email"
                     >
                       Email
                     </label>
                     <input
-                      class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                       className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                       type="email"
                       name="email"
                       id="email"
                       placeholder="Email"
                     />
                   </div>
-                  <div class="grid w-full  items-center gap-1.5">
+                  <div  className="grid w-full  items-center gap-1.5">
                     <label
-                      class="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                       className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                       for="phone_number"
                     >
                       Phone number
                     </label>
                     <input
-                      class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                       className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                       type="tel"
                       name="phone"
                       id="phone_number"
                       placeholder="Phone number"
                     />
                   </div>
-                  <div class="grid w-full  items-center gap-1.5">
+                  <div  className="grid w-full  items-center gap-1.5">
                     <label
-                      class="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                       className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                       for="message"
                     >
                       Message
                     </label>
                     <textarea
-                      class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                       className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                       id="message"
                       name="message"
                       placeholder="Leave us a message"
@@ -124,7 +124,7 @@ export default function ContactPage(){
                   <button
                   onClick={notify}
                     type="submit"
-                    class="w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                     className="w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                   >
                     Send Message
                   </button>
