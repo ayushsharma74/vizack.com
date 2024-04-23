044dcea18858a29da588402bd21cb8563e31a36d
Gajender <Gajendrasuman868@gmail.com>
2024-04-23 14:30:35 +0000

    sdfs

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -10,51 +10,42 @@ import { useState } from "react"
 // import asset1 from "/assets/images/asset1.jpg"
 
 export default function Blogs() {
-    const [data, setData] = useState("404 Page Not Found")
-    // Octokit Authorization
-    const git_token = "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";
-    const { Octokit } = require("@octokit/rest");
-    const octokit = new Octokit({
-    auth: git_token,
-    });
+const octokit = new Octokit({
+  auth: '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', // Your GitHub token
+});
 
-    // Read File Content
-    async function readFileFromRepo(owner, repo, path) {
-        try {
-            const response = await octokit.repos.getContent({
-            owner,
-            repo,
-            path,
-            });
-        
-            // Ensure the response is a file and not a directory
-            if (Array.isArray(response.data) || !response.data.content) {
-            throw new Error("Specified path is not a file");
-            }
-        
-            // Decode base64 content
-            let content = Buffer.from(response.data.content, "base64").toString();
-            content = JSON.parse(content)
-            return content;
-        } catch (error) {
-            console.error("Error reading file:", error.message);
-            throw error;
+const ReadFileFromRepo = () => {
+  const [data, setData] = useState("404 Page Not Found");
+
+  useEffect(() => {
+    const owner = "Gajendrasuman";
+    const repo = "vizack-blogs";
+    const path = "blogs.json";
+
+    async function fetchData() {
+      try {
+        const response = await octokit.repos.getContent({ owner, repo, path });
+
+        // Ensure the response is a file and not a directory
+        if (!Array.isArray(response.data) && response.data.content) {
+          // Decode base64 content
+          let content = Buffer.from(response.data.content, "base64").toString();
+          content = JSON.parse(content);
+          setData(content);
+          console.log("File content:", content);
+        } else {
+          throw new Error("Specified path is not a file");
         }
-        }
-        
-        // Example usage:
-        const owner = "Gajendrasuman";
-        const repo = "vizack-blogs";
-        const path = "blogs.json";
-        
-        readFileFromRepo(owner, repo, path)
-        .then(content => {
-            setData(content)
-            console.log("File content:", content);
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        });
+      } catch (error) {
+        console.error("Error reading file:", error.message);
+        setData("Error: " + error.message);
+      }
+    }
+
+    fetchData();
+
+  }, []);
+
     return (
         <>
         <div className="bg-default w-full min-h-10 h-fit py-2 flex flex-wrap items-center px-5" >
