cc42295a0801d64f8f49a43facfacb9a92022bac
Gajender <Gajendrasuman868@gmail.com>
2024-04-23 14:50:30 +0000

    fsdksdf

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -5,7 +5,7 @@ import RecentPost from "@/components/blogComponents/RecentPost"
 import Sidebar from "@/components/blogComponents/Sidebar"
 import "./blogs.css"
 import Link from "next/link"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Octokit } from "@octokit/rest"
 
 // import asset1 from "/assets/images/asset1.jpg"
