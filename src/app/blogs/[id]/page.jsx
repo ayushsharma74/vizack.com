9133ffe7d36287c7e7e78a619637174d31778108
Gajender <Gajendrasuman868@gmail.com>
2024-04-25 08:58:47 +0000

    rollback

--- src/app/blogs/[id]/page.jsx
+++ src/app/blogs/[id]/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import './blogpage.css'
+// import './blogpage.css'
 import BlogTitle from '@/components/blogComponents/BlogTitle'
 import Image from 'next/image'
 import Link from 'next/link'
