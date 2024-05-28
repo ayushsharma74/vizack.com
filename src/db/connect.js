3516fe50453651b437bea8d12ddb23168b24959c
Ayush Sharma <ayush.sharma74892@gmail.com>
2024-05-28 06:22:37 +0000

    testing

--- src/db/connect.js
+++ src/db/connect.js
@@ -4,7 +4,8 @@ async function dbConnect() {
    
 
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URI || "")
+        // const db = await mongoose.connect(process.env.MONGODB_URI || "")
+        const db = await mongoose.connect("mongodb+srv://ayushsharma769287:admin@cluster0.9qupizd.mongodb.net/vizackblog")
         console.log("Connected to MongoDB");
         
     } catch (error) {
